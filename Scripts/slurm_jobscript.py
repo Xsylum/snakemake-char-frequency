fb393208ec3f84ea8fd7be36783c0176e87e8978
Xsylum <fleuryxander@gmail.com>
2025-02-11 19:56:08 +0000

    Fix profile not using correct snakemake executor

--- Scripts/slurm_jobscript.py
+++ Scripts/slurm_jobscript.py
@@ -13,11 +13,15 @@ import logging
 
 from snakemake.utils import read_job_properties
 
+logfile_path = os.path.expanduser("~/slurm_jobscript.log")
 logging.basicConfig(format="%(asctime)s - %(message)s",
                     datefmt="%d-%b-%y %H:%M:%S",
                     level=logging.INFO,
-                    filename="slurm_jobscript.log",
+                    filename=logfile_path,
                     filemode="a")
+logger = logging.getLogger('slurm_logger')
+logger.setLevel(logging.INFO)
+print("testing\n")
 
 jobscript = sys.argv[-1]
 job_properties = read_job_properties(jobscript)
@@ -26,7 +30,7 @@ timestamp = datetime.datetime.now().strftime("%Y%m%d.%H%M%S")
 
 threads = job_properties["threads"]
 jobid = job_properties["jobid"]
-rulename = job_properties["rule"]
+rulename = job_properties["rule"]   # This will be a problem if we desire to submit job groups!
 cluster_nodes = job_properties["resources"]["nodes"]
 cluster_ntasks = job_properties["resources"]["ntasks"]
 cluster_account = job_properties["resources"]["slurm_account"]
@@ -57,11 +61,12 @@ cmdline = f"""sbatch -N {cluster_nodes} -n {cluster_ntasks} -p {cluster_partitio
                      -J {rulename}.{jobid} \
                      -o logs/{rulename}.{jobid}.{timestamp}.log \
                      -e logs/{rulename}.{jobid}.{timestamp}.err \
-                     --mail-type={cluster_mail_type} --mail-user={cluster_mail_user} \
+                     --mail-user={cluster_mail_user} --mail-type={cluster_mail_type} \
                      {resources_mem} {clusters} {qos} {jobscript}"""
+
 print(cmdline)
-logging.info(f"rule: {rulename}")
-logging.info(f"job_properties['resources']: {job_properties['resources']}")
-logging.info(f"command line: {cmdline}")
-logging.info("")
+logger.info(f"rule: {rulename}")
+logger.info(f"job_properties['resources']: {job_properties['resources']}")
+logger.info(f"command line: {cmdline}")
+logger.info("test")
 os.system(cmdline)
